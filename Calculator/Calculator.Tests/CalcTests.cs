7be5550490554619714985488f78b0defa9c04aa
André <andrerrr@outlook.de>
2019-04-09 10:42:53 +0200

    Tests

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Calc_Sum_3_and_4_results_7()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            int result = calc.Sum(3, 4);
+
+            //Assert
+            Assert.AreEqual(7, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_0_and_0_results_0()
+        {
+            //Arrange
+            Calc calc = new Calc();
+
+            //Act
+            int result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_MAX_and_1_throw_OverFlowException()
+        {
+            Calc calc = new Calc();
+
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+
+        }
+
+        [TestMethod]
+        public void Calc_IsWeekend()
+        {
+            Calc calc = new Calc();
+
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 4, 8);
+                Assert.IsFalse(calc.IsWeekend()); //Mo
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 4, 9);
+                Assert.IsFalse(calc.IsWeekend()); //Di
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 4, 10);
+                Assert.IsFalse(calc.IsWeekend()); //Mi
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 4, 11);
+                Assert.IsFalse(calc.IsWeekend()); //Do
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 4, 12);
+                Assert.IsFalse(calc.IsWeekend()); //Fr
+
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 4, 13);
+                Assert.IsTrue(calc.IsWeekend()); //Sa
+
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 4, 14);
+                Assert.IsTrue(calc.IsWeekend()); //So
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Calculator.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Calculator.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("530717c8-5c43-491f-a22a-6e3a2d12145c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
